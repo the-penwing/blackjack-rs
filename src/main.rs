46ca0b8a278bde098c4b1bdef69fd3b63fc60bbc
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-07-21 09:08:49 +1000

    added clear screen & read input utility to main

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,26 @@
+use blackjack_rs;
+use std::{
+  io::{self, Write},
+  process::Command,
+};
+
+fn get_input(prompt: &str) -> String {
+  print!("{}", prompt);
+  io::stdout().flush().unwrap();
+
+  let mut input = String::new();
+  io::stdin()
+    .read_line(&mut input)
+    .expect("Failed to read line");
+  input.trim().to_string()
+}
+
+fn clear() {
+  if cfg!(target_os = "windows") {
+    Command::new("cls");
+  } else {
+    Command::new("clear");
+  }
+}
+
 fn main() {}
