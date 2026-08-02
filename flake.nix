8147fef0720885c7e707888fc231be02830a4289
Ben van Leeuwen <benvanleeuwen01@gmail.com>
2026-08-02 21:59:42 +1000

    updated flake

--- flake.nix
+++ flake.nix
@@ -9,7 +9,11 @@
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-      perSystem = {system, ...}: let
+      perSystem = {
+        system,
+        pkgs,
+        ...
+      }: let
         pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [(import inputs.rust-overlay)];
@@ -25,8 +29,23 @@
           RUSTFLAGS = "-C linker=clang -C link-arg=-fuse-ld=mold";
         };
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        ishArgs =
+          commonArgs
+          // {
+            CARGO_BUILD_TARGET = "i686-unknown-linux-musl";
+            CARGO_TARGET_I686_UNKNOWN_LINUX_MUSL_RUSTFLAGS = "-C target-cpu=pentium4";
+            RUSTFLAGS = "";
+            nativeBuildInputs = with pkgs; [cargo-zigbuild zig];
+            cargoExtraArgs = "--target i686-unknown-linux-musl";
+            cargoBuildCommand = "cargo zigbuild";
+            preBuild = "export HOME=$TMPDIR";
+          };
+
+        ishArtifacts = craneLib.buildDepsOnly ishArgs;
       in {
         packages.default = craneLib.buildPackage (commonArgs // {inherit cargoArtifacts;});
+        packages.ish = craneLib.buildPackage (ishArgs // {cargoArtifacts = ishArtifacts;});
         devShells.default = craneLib.devShell {
           packages = with pkgs; [just cargo-zigbuild zig];
           shellHook = ''
